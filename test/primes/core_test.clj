4136435a1c4625a88efd5eaed1e5aebb1d9b01ca
Kaylee Moser <kay.moser@me.com>
2015-10-25 11:42:06 -0700

    test that checks that first 10 primes get properly labeled

--- test/primes/core_test.clj
+++ test/primes/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [primes.core :refer :all]))
 
+(def prime_nums [2 3 5 7 11 13 17 19 23 27])
 (deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+  (testing "Tests first 10 prime numbers for prime-ness."
+    (loop [i 0]
+      (if (< i (- (count prime_nums) 1))
+        (do (is (is-prime (get prime_nums i)))
+             (recur (inc i)))
+        (println "all good")))))
