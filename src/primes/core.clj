30e4f987a784cd0fcacce59a497073b9acc83005
Kaylee Moser <kay.moser@me.com>
2015-10-24 22:19:26 -0700

    is_prime algorithm determines if given number is a prime number

--- src/primes/core.clj
+++ src/primes/core.clj
@@ -1,7 +1,25 @@
 (ns primes.core
   (:gen-class))
 
+(defn is_prime
+  "returns true if number is prime"
+  [number]
+  (def root (int (Math/sqrt number)))
+  (loop [iteration 2]
+    (if (= iteration root)
+      true
+      (if (= (rem number iteration) 0)
+        false
+        (recur (inc iteration))))))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (if (is_prime 13)
+    (println "13 is prime!")
+    (println "13 is not prime!")))
+
+
+
+
+
