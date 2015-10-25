372c3b2a7feedd1ed6f7782712181a14d8a4c9d9
Kaylee Moser <kay.moser@me.com>
2015-10-25 10:59:19 -0700

    finds first 10 primes

--- src/primes/core.clj
+++ src/primes/core.clj
@@ -5,19 +5,31 @@
   "returns true if number is prime"
   [number]
   (def root (int (Math/sqrt number)))
+  ;; if number is divisible by an number between 2 and it's sqrt,
+  ;; it is not a prime
   (loop [iteration 2]
-    (if (= iteration root)
+    (if (> iteration root)
       true
       (if (= (rem number iteration) 0)
         false
         (recur (inc iteration))))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Take in number of primes, find primes, print primes."
   [& args]
-  (if (is_prime 13)
-    (println "13 is prime!")
-    (println "13 is not prime!")))
+  
+  ;; define vector of primes
+  (def primes [])
+
+  ;; loop starting from first prime number user-specified times
+  ;; if a number is prime, add it to the prime numbers vector
+  (loop [number 2]
+    (when (< (count primes) 10)
+      (do (when (is_prime number)
+            (do (def primes (conj primes number))
+                 (println number)))
+          (recur (inc number))))))
+
 
 
 
