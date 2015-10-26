6c867c6ed409c8f436579511bd7915c21ab9f08d
Kaylee Moser <kay.moser@me.com>
2015-10-26 10:32:43 -0700

    testing for non-primes and 0

--- test/primes/core_test.clj
+++ test/primes/core_test.clj
@@ -2,11 +2,26 @@
   (:require [clojure.test :refer :all]
             [primes.core :refer :all]))
 
-(def prime_nums [2 3 5 7 11 13 17 19 23 27])
-(deftest a-test
+(def prime-nums [2 3 5 7 11 13 17 19 23 27])
+(deftest prime-test
   (testing "Tests first 10 prime numbers for prime-ness."
     (loop [i 0]
-      (if (< i (- (count prime_nums) 1))
-        (do (is (is-prime (get prime_nums i)))
-             (recur (inc i)))
-        (println "all good")))))
+      (when (< i (- (count prime-nums) 1))
+        (do (is (is-prime (get prime-nums i)))
+             (recur (inc i)))))))
+
+;; just in case something happens where is-prime is 
+;; passed a 0
+(deftest zero-test
+  (testing "tests that 0 is not returned as prime"
+    (is (= (is-prime 0) false))))
+
+;; includes case of 1, even though program shouldn't call it
+(def not-prime-nums [1 4 6 8 9 10 12 14 15 16])
+(deftest not-prime-test
+  (testing "Tests first 10 non-prime numbers for non-prime-ness"
+    (loop [j 0]
+      (when (< j (- (count not-prime-nums) 1))
+        (do (is (= (is-prime (get not-prime-nums j)) false))
+            (recur (inc j)))))))
+
